8cc6efe2405fd0620336b9b0827c3ef20fe82cdd
Radek Szymczyszyn <lavrin@gmail.com>
2015-10-13 22:04:02 +0200

    Implement msgpack fileio server

--- nv.escript
+++ nv.escript
@@ -6,43 +6,17 @@
 -mode(compile).
 
 main(_Args) ->
-    {ok, F} = file:open("/tmp/out", write),
-    dbg:tracer(process, {fun dbg:dhandler/2, F}),
-    dbg:p(all, call),
-    dbg:tpl(?MODULE, x),
     code:add_paths( [base_dir(escript:script_name())] ),
     code:add_paths( deps_dirs(escript:script_name()) ),
-    print("alive~n"),
-    loop(standard_io, <<>>).
-
-loop(Handle, Buffer) ->
-    case file:read(Handle, 1) of
-        {ok, Data} ->
-            print("data: ~p~n", [Data]),
-            {ok, NewBuffer} = process_data(cat(Buffer, Data)),
-            loop(Handle, NewBuffer);
-        eof ->
-            print("eof");
-        {error, R} ->
-            print("error: ~p~n", [R])
+    {ok, Pid} = msgpack_rpc_fileio_server:start_link(standard_io, undef, []),
+    MRef = erlang:monitor(process, Pid),
+    receive
+        {'DOWN', MRef, process, _Object, Info} ->
+            print("io server down: ~p~n", [Info])
     end.
 
-process_data(Data) ->
-    case msgpack:unpack_stream(Data) of
-        {error, incomplete} ->
-            {ok, Data};
-        {error, Reason} ->
-            {error, Reason};
-        {Term, Remain} ->
-            print("unpacked: ~p~n", [Term]),
-            {ok, Remain}
-    end.
-
-cat(B1, B2) ->
-    list_to_binary([B1, B2]).
-
-print(Text) ->
-    print(Text, []).
+%print(Text) ->
+%    print(Text, []).
 
 print(Fmt, Args) ->
     %io:format(Fmt, Args).
---
+++ src/msgpack_rpc_fileio_server.erl
@@ -0,0 +1,96 @@
+-module(msgpack_rpc_fileio_server).
+
+-export([start_link/3]).
+
+%% Internal
+-export([init/3,
+         parse_request/1]).
+
+-include_lib("msgpack_rpc/include/msgpack_rpc.hrl").
+
+-record(state, {iodev, buffer, module}).
+
+start_link(IoDev, Module, Opts) ->
+    Pid = spawn_link(?MODULE, init, [IoDev, Module, Opts]),
+    {ok, Pid}.
+
+init(IoDev, Module, _Opts) ->
+    loop(#state{iodev = IoDev,
+                buffer = <<>>,
+                module = Module}).
+
+loop(#state{} = S) ->
+    case file:read(S#state.iodev, 1) of
+        {ok, Data} ->
+            parse_request(S#state{buffer = cat(S#state.buffer, Data)});
+        eof ->
+            ok;
+        {error, Reason} ->
+            error_logger:error_msg("read error: ~p", [Reason]),
+            error(Reason)
+    end.
+
+cat(B1, B2) ->
+    list_to_binary([B1, B2]).
+
+parse_request(#state{} = S) ->
+    #state{buffer = Buffer, module = Module} = S,
+    case msgpack:unpack_stream(Buffer) of
+        {[?MP_TYPE_REQUEST, CallID, M, Argv], Remain} ->
+            spawn_request_handler(S#state.iodev, CallID, Module, M, Argv),
+            parse_request(S#state{buffer = Remain});
+        {[?MP_TYPE_NOTIFY, M, Argv], Remain} ->
+            spawn_notify_handler(Module, M, Argv),
+            parse_request(S#state{buffer = Remain});
+        {error, incomplete} ->
+            loop(S);
+        {error, Reason} ->
+            error_logger:error_msg("unpack error: ~p", [Reason]),
+            error(Reason)
+    end.
+
+spawn_request_handler(IoDev, CallID, Module, M, Argv) ->
+    Pid = self(),
+    F = fun() ->
+                Ref = erlang:monitor(process, Pid),
+                Method = binary_to_existing_atom(M, latin1),
+                Prefix = [?MP_TYPE_RESPONSE, CallID],
+                try
+                    Result = erlang:apply(Module,Method,Argv),
+                    reply(IoDev, {reply, msgpack:pack(Prefix ++ [nil, Result])})
+                catch
+                    error:Reason ->
+                        error_logger:error_msg("no such method: ~p / ~p", [Method, Reason]),
+                        ReplyBin = msgpack:pack(Prefix ++ [error2binary(Reason), nil]),
+                        reply(IoDev, {reply, ReplyBin});
+                    Class:Throw ->
+                        Error = lists:flatten(io_lib:format("~p:~p", [Class, Throw])),
+                        error_logger:error_msg("(~p)~s", [self(), Error]),
+                        case msgpack:pack(Prefix ++ [Error, nil]) of
+                            {error, _Reason} ->
+                                reply(IoDev, {reply, ["internal error", nil]});
+                            Binary when is_binary(Binary) ->
+                                reply(IoDev, {reply, Binary})
+                        end
+                end,
+                erlang:demonitor(Ref)
+        end,
+    spawn(F).
+
+reply(IoDev, {reply, Data}) ->
+    file:write(IoDev, Data).
+
+spawn_notify_handler(Module, M, Argv) ->
+    spawn(fun() ->
+                  Method = binary_to_existing_atom(M, latin1),
+                  try
+                      erlang:apply(Module, Method, Argv)
+                  catch
+                      Class:Throw ->
+                          error_logger:error_msg("~p ~p:~p", [?LINE, Class, Throw])
+                  end
+          end).
+
+-spec error2binary(atom())->binary().
+error2binary(undef) -> <<"undef">>;
+error2binary(function_clause) -> <<"function_clause">>.
